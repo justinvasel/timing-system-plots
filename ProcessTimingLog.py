439c7e60ffced7d2e75bc9c5898f4db9f87611ba
Justin Vasel <justin.vasel@gmail.com>
2018-02-04 03:27:10 -0500

    Remove commented junk

--- ProcessTimingLog.py
+++ ProcessTimingLog.py
@@ -9,10 +9,6 @@ import Database
 parser = argparse.ArgumentParser(description = 'Timing System Log File Parser')
 
 parser.add_argument('logfile', help = 'Path to logfile to process')
-
-# parser.add_argument('-f', action = 'store', dest = 'logfile',
-#                     help = 'Path to logfile to process')
-
 parser.add_argument('--version', action = 'version', version = '%(prog)s {}'.format(__version__))
 
 results = parser.parse_args()
