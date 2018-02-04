525c8f416954252bc1be62337ff7c1ea2ada6a54
Justin Vasel <justin.vasel@gmail.com>
2018-02-04 03:24:13 -0500

    Add argument parsing

--- ProcessTimingLog.py
+++ ProcessTimingLog.py
@@ -1,3 +1,18 @@
+__version__ = 'v0.1.0'
+
+# Python modules
+import argparse
+# App-specific modules
 import Database
 
-# CONFIGURATION
+# Argument parsing
+parser = argparse.ArgumentParser(description = 'Timing System Log File Parser')
+
+parser.add_argument('logfile', help = 'Path to logfile to process')
+
+# parser.add_argument('-f', action = 'store', dest = 'logfile',
+#                     help = 'Path to logfile to process')
+
+parser.add_argument('--version', action = 'version', version = '%(prog)s {}'.format(__version__))
+
+results = parser.parse_args()
