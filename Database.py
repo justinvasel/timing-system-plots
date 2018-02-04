3bdd8cc1a0459f5e4c34a9d44249d5604647ec5c
Justin Vasel <justin.vasel@gmail.com>
2018-02-04 03:05:17 -0500

    Move database stuff into its own file

---
+++ Database.py
@@ -0,0 +1,53 @@
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# CONFIGURATION
+SQL_DATABASE = 'sqlite:///data/timing.db'
+
+engine = create_engine(SQL_DATABASE, echo = True)
+Base = declarative_base()
+
+
+class Logfile(Base):
+    __tablename__ = 'logfiles'
+
+    id = Column(Integer, primary_key = True)
+    filename = Column(String, nullable = False)
+
+
+class SpillType(Base):
+    __tablename__ = 'spill_types'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable = False)
+    evtcode = Column(Integer, nullable = False)
+
+
+class Spill(Base):
+    __tablename__ = 'spills'
+
+    id = Column(Integer, primary_key = True)
+    spill_type_id = Column(Integer, ForeignKey('spill_types.id'), nullable = False)
+    time_spillserver = Column(DateTime)
+    time_fwd_near = Column(DateTime)
+    time_rec_near = Column(DateTime)
+    time_fwd_far = Column(DateTime)
+    time_rec_far = Column(DateTime)
+
+
+class Heartbeat(Base):
+    __tablename__ = 'heartbeats'
+
+    id = Column(Integer, primary_key = True)
+    time = Column(DateTime)
+    app = Column(String)
+
+class TDU(Base):
+    __tablename__ = 'tdus'
+
+    id = Column(Integer, primary_key = True)
+    hostname = Column(String)
+
+
+Base.metadata.create_all(engine)
--- ProcessTimingLog.py
+++ ProcessTimingLog.py
@@ -1,11 +1,3 @@
-from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Date, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+import Database
 
 # CONFIGURATION
-SQL_DATABASE = 'sqlite:///data/timing.db'
-
-engine = create_engine(SQL_DATABASE, echo = True)
-Base = declarative_base()
-
-Base.metadata.create_all(engine)
