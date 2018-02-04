e04cd1d1c0e460795e14c8cc5ef54fec1ca6bfe8
Justin Vasel <justin.vasel@gmail.com>
2018-02-04 03:56:03 -0500

    Add loop over log file and extraction of message type

--- ProcessTimingLog.py
+++ ProcessTimingLog.py
@@ -2,7 +2,9 @@ __version__ = 'v0.1.0'
 
 # Python modules
 import argparse
+import re
 # App-specific modules
+import Config as config
 import Database
 
 # Argument parsing
@@ -12,3 +14,32 @@ parser.add_argument('logfile', help = 'Path to logfile to process')
 parser.add_argument('--version', action = 'version', version = '%(prog)s {}'.format(__version__))
 
 results = parser.parse_args()
+
+# Open log file
+log = open(results.logfile, 'r')
+lines = log.readlines()
+
+# Loop over each line of the log file
+for lineno in range(0, len(lines)):
+    line = lines[lineno].rstrip()
+
+    # Get the message type
+    match = re.match( r'%MSG-i (.*): .*', line, re.M|re.I)
+    if match == None:
+        continue
+    msgType = match.group(1)
+
+    if config.DEBUG == True:
+        print('Found MSG : {}'.format(msgType))
+
+    # Perform an appropriate action depending on the message type
+    if msgType == 'SpillSent':
+        print('I will process a {} message type'.format(msgType))
+
+    if msgType == 'TimeDrift':
+        print('I will process a {} message type'.format(msgType))
+
+    # print "Next line: ", lines[lineno + 1].rstrip()
+
+    if lineno > 10:
+        break
