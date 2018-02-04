24833b6b29e6e627ac0ef760de78f21c120ecf1f
Justin Vasel <justin.vasel@gmail.com>
2018-02-04 07:20:22 -0500

    First attempt at generating plots

--- Config.py
+++ Config.py
@@ -2,3 +2,6 @@ DEBUG = False
 
 # DATABASE LOCATION
 SQL_DATABASE = 'sqlite:///data/timing.db'
+
+# PLOT OUTPUT DIRECTORY
+PLOT_OUT_DIR = 'out'
---
+++ GenerateTimingPlots.py
@@ -0,0 +1,13 @@
+# /////////////////////////////////////////////////////////////////////////////
+#  NOvA TIMING SYSTEM PLOTTER
+#  \brief   A script to produce timing system plots
+#  \author  Justin Vasel <jvasel@indiana.edu>
+#  \date    February 2018
+# /////////////////////////////////////////////////////////////////////////////
+
+import code
+
+import Config as config
+from Plots import SpillPlots
+
+SpillPlots.SpillCounts()
