24b93fdcc4d9c79382a6fa588771b1f0452b5870
Justin Vasel <justin.vasel@gmail.com>
2019-01-25 09:07:38 -0500

    Align code blocks

--- Database.py
+++ Database.py
@@ -14,70 +14,69 @@ session = Session()
 class Logfile(Base):
     __tablename__ = 'logfiles'
 
-    id = Column(Integer, primary_key = True)
-    filename = Column(String, nullable = False, unique = True)
+    id               = Column(Integer, primary_key = True)
+    filename         = Column(String,  nullable = False, unique = True)
 
 class Detector(Base):
     __tablename__ = 'detectors'
-    
-    id = Column(Integer, primary_key = True)
-    fullname  = Column(String, nullable = False)
-    shortname = Column(String, nullable = False)
+
+    id               = Column(Integer, primary_key = True)
+    fullname         = Column(String,  nullable = False)
+    shortname        = Column(String,  nullable = False)
 
 class SpillType(Base):
     __tablename__ = 'spill_types'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String, nullable = False)
-    evtcode = Column(Integer, nullable = False)
+    id               = Column(Integer, primary_key = True)
+    name             = Column(String,  nullable = False)
+    evtcode          = Column(Integer, nullable = False)
 
 class Spill(Base):
     __tablename__ = 'spills'
 
-    id = Column(Integer, primary_key = True)
-    spill_type_id = Column(Integer, ForeignKey('spill_types.id'), nullable = False)
-    time = Column(Integer)
+    id               = Column(Integer, primary_key = True)
+    spill_type_id    = Column(Integer, ForeignKey('spill_types.id'), nullable = False)
+    time             = Column(Integer)
 
 class SpillReceived(Base):
     __tablename__ = 'spillsReceived'
-    
-    id = Column(Integer, primary_key = True)
+
+    id               = Column(Integer, primary_key = True)
     time_spillserver = Column(Integer, ForeignKey('spills.time'))
-    detector_id = Column(Integer, ForeignKey('detectors.id'))
-    partition = Column(Integer)
-    time = Column(Integer)
+    detector_id      = Column(Integer, ForeignKey('detectors.id'))
+    partition        = Column(Integer)
+    time             = Column(Integer)
 
 class SpillForwarded(Base):
     __tablename__ = 'spillsForwarded'
-    
-    id = Column(Integer, primary_key = True)
-    spill_type_id = Column(Integer, ForeignKey('spill_types.id'), nullable = False)
+
+    id               = Column(Integer, primary_key = True)
+    spill_type_id    = Column(Integer, ForeignKey('spill_types.id'), nullable = False)
     time_spillserver = Column(Integer, ForeignKey('spills.time'))
-    detector_id = Column(Integer, ForeignKey('detectors.id'))
-    timeBegin = Column(Integer)
-    timeEnd = Column(Integer)
-    
+    detector_id      = Column(Integer, ForeignKey('detectors.id'))
+    timeBegin        = Column(Integer)
+    timeEnd          = Column(Integer)
 
 class Heartbeat(Base):
     __tablename__ = 'heartbeats'
 
-    id = Column(Integer, primary_key = True)
-    time = Column(Integer)
-    app = Column(String)
+    id               = Column(Integer, primary_key = True)
+    time             = Column(Integer)
+    app              = Column(String)
 
 class TDU(Base):
     __tablename__ = 'tdus'
 
-    id = Column(Integer, primary_key = True)
-    hostname = Column(String)
+    id               = Column(Integer, primary_key = True)
+    hostname         = Column(String)
 
 class TCR(Base):
     __tablename__ = 'tcrs'
 
-    id = Column(Integer, primary_key = True)
-    time = Column(Integer)
-    tdu_id = Column(Integer, ForeignKey('tdus.id'), nullable = False)
-    delta = Column(Integer, nullable = False)
+    id               = Column(Integer, primary_key = True)
+    time             = Column(Integer)
+    tdu_id           = Column(Integer, ForeignKey('tdus.id'), nullable = False)
+    delta            = Column(Integer, nullable = False)
 
 Base.metadata.create_all(engine)
 
@@ -85,6 +84,6 @@ Base.metadata.create_all(engine)
 # Set up static tables
 if session.query(Detector).filter(Detector.id == 1).first() == None:
     session.add(Detector(id = 1, fullname = 'NearDet', shortname = 'ND'))
-    
+
 if session.query(Detector).filter(Detector.id == 2).first() == None:
     session.add(Detector(id = 2, fullname = 'FarDet',  shortname = 'FD'))
